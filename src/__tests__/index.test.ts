33a0413becfa1bc33f1aef50bc94517187e743a9
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-12 05:44:49 +0500

    github actions

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,15 @@
+import { BlueBase } from "@bluebase/core";
+import Plugin from "../index";
+
+/**
+ * Mocking expo Library
+ */
+
+jest.mock("expo", () => ({}));
+
+test("Plugin should be correctly registered", async () => {
+  jest.mock("expo", () => jest.fn());
+  const BB = new BlueBase();
+  await BB.Plugins.register(Plugin);
+  expect(BB.Plugins.has("hello-worl")).toBeTruthy();
+});

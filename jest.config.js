aa550f4c84fff42a682b2ab33d07a9447266472e
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-12 08:49:22 +0500

    lowered coverqage thresolds

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 const configs = require('@bluebase/code-standards/jest.config');
 
 module.exports = Object.assign(configs, {
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"]  
+	setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+	coverageThreshold: {
+		global: {
+			branches: 5,
+			functions: 5,
+			lines: 5,
+			statements: 5,
+		},
+	},
 });

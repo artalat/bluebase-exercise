3427b677a819841cc51421fe4c9e27227f5626b7
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-16 00:28:27 +0500

    fix version path

--- .github/actions/expo-deploy/src/github-upload-release-asset.js
+++ .github/actions/expo-deploy/src/github-upload-release-asset.js
@@ -1,14 +1,21 @@
 const api = require('./github-api');
 const githubEvent = require('./github-event');
-const { GITHUB_REPOSITORY_NAME, GITHUB_TOKEN } = require('./constants');
-const version = require('./package.json').version;
+const {
+	GITHUB_REPOSITORY_NAME,
+	GITHUB_TOKEN,
+	REPO_DIRECTORY,
+} = require('./constants');
 const download = require('siwi-file');
 const ghReleaseAssets = require('gh-release-assets');
+const fs = require('fs');
+const path = require('path');
 
 const uploadReleaseAsset = async (assetUrl, platform) => {
 	const event = await githubEvent();
 	const owner = event.repository.owner.login;
 	const repo = event.repository.name;
+
+	const version = getVersion();
 
 	if (!version) {
 		throw Error('No version found in package.json');
@@ -52,4 +59,10 @@ const ghReleaseAssetsAsync = async opts => {
 	});
 };
 
+const getVersion = () => {
+	const contents = fs.readFileSync(path.join(REPO_DIRECTORY, 'package.json'));
+	const pkg = JSON.parse(contents);
+
+	return pkg.version;
+};
 module.exports = uploadReleaseAsset;

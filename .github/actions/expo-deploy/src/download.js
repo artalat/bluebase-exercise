42b374aff35a3f23d6d88184081751f698f2956e
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-16 11:06:35 +0500

    more logs

--- .github/actions/expo-deploy/src/download.js
+++ .github/actions/expo-deploy/src/download.js
@@ -8,7 +8,9 @@ const file = require('siwi-file').file;
  * @param {*} filename
  */
 async function download(uri, filename) {
+	console.log('returning promise');
 	return new Promise((resolve, reject) => {
+		console.log('inside promise');
 		var protocol = url.parse(uri).protocol.slice(0, -1);
 		var onError = function(e) {
 			fs.unlink(filename);
@@ -17,10 +19,13 @@ async function download(uri, filename) {
 		require(protocol)
 			.get(uri, function(response) {
 				if (response.statusCode >= 200 && response.statusCode < 300) {
+					console.log('now downloading', uri);
 					file(uri, 'app.apk').catch(e => console.log('err', e));
 				} else if (response.headers.location) {
+					console.log('whoops we were redirected', response.headers.location);
 					resolve(download(response.headers.location, filename));
 				} else {
+					console.log('download error', response.statusMessage);
 					reject(new Error(response.statusCode + ' ' + response.statusMessage));
 				}
 			})
